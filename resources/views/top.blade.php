26767e4fa28601a02abe2c00029273b7f96b7f6e
egapool <egpersent@gmail.com>
2017-11-07 18:28:19 +0900

    css,js,mdファイル引越し

---
+++ resources/views/top.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>Let's Study Programing</title>
+	<meta name="format-detection" content="telephone=no">
+	<meta name="Keywords" content="">
+	<meta name="description" content="">
+	<meta name="referrer" content="unsafe-url">
+	<meta http-equiv="content-language" content="ja">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href="/css/reset.css" rel="stylesheet"></link>
+	<link href="/css/github-markdown.css" rel="stylesheet"></link>
+	<!-- <link href="/css/hightlightstyles/agate.css" rel="stylesheet"></link> -->
+	<!-- <link href="/css/hightlightstyles/dracula.css" rel="stylesheet"></link> -->
+	<!-- <link href="/css/hightlightstyles/gruvbox-dark.css" rel="stylesheet"></link> -->
+	<link href="/css/hightlightstyles/monokai.css" rel="stylesheet"></link>
+	<link href="/css/style.css" rel="stylesheet"></link>
+	<!-- <script src="/js/syntaxhighlighter.js"></script> -->
+
+</head>
+<body>
+	<div class="container-fulid">
+		<div class="row">
+			<div id="side-menu" class="col-sm-3 col-md-2">
+				<p class="link-top"><a href="/">TOP</a></p>
+				<ul class="chapers">
+					<li>
+						<p class="chaper">1.HTMLとCSSの基礎</p>
+						<ul class="titles">
+							<li><a class="arrow ku-arrow" href="/1.HTMLとCSSの基礎/1-1.WebサイトはHTMLで書かれている">1-1.WebサイトはHTMLで書かれている</a></li>
+							<li><a class="arrow ku-arrow" href="/1.HTMLとCSSの基礎/1-2.HTMLの様々なタグを使う">1-2.HTMLの様々なタグを使う</a></li>
+							<li><a class="arrow ku-arrow" href="/1.HTMLとCSSの基礎/1-3.CSSでHTMLを装飾する">1-3.CSSでHTMLを装飾する</a></li>
+							<li><a class="arrow ku-arrow" href="/1.HTMLとCSSの基礎/1-4.CSSでスペースを調整する">1-4.CSSでスペースを調整する</a></li>
+							<li><a class="arrow ku-arrow" href="/1.HTMLとCSSの基礎/6.CSSで余白の制御">6.CSSで余白の制御</a></li>
+							<li><a class="arrow ku-arrow" href="/1.HTMLとCSSの基礎/7.要素を横に並べる">7.要素を横に並べる</a></li>
+						</ul>
+					</li>
+					<li>
+						<p class="chaper">2.2カラムレイアウトのコーディング</p>
+						<ul class="titles">
+							<li><a class="arrow ku-arrow" href="/2.2カラムレイアウトのコーディング/1.２カラムのレイアウトを作る">1.２カラムのレイアウトを作る</a></li>
+						</ul>
+					</li>
+					<li>
+						<p class="chaper">3.PHPの基礎</p>
+						<ul class="titles">
+							<li><a class="arrow ku-arrow" href="/3.PHPの基礎/3-1.変数、ループ、if分岐">3-1.変数、ループ、if分岐</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<div class="col-sm-9 col-md-10">コンテンツ</div>
+		</div>
+	</div>
+	<footer>
+
+	</footer>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+	<script>hljs.initHighlightingOnLoad();</script>
+</body>
+</html>
